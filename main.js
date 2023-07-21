d3f18849619c84bc60e5f621ba2527fc82f70712
suusara <smataeva09@gmail.com>
2023-07-21 23:11:36 +0600

    add JS

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const copyButtons = document.querySelectorAll(".copyButton");
+const copyStatuses = document.querySelectorAll(".copyStatus");
+
+copyButtons.forEach((button, index) => {
+  const textToCopy = button.getAttribute("data-text");
+
+  button.addEventListener("click", () => {
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        copyStatuses[index].textContent = "Text Copied";
+      })
+      .catch((error) => {
+        copyStatuses[index].textContent = `Error while copying: ${error}`;
+      });
+  });
+});
 
